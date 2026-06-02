2c7daa482891aa5cefb320e429b85f775df2ca6d
rishabh248-bit <rbks0111@gmail.com>
2026-06-02 20:59:02 +0530

    Implement containsDuplicate function in Q2.cpp

---
+++ Week1/Day2/Q2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> dup;
+        for(int n : nums){
+            if(dup.count(n)){
+                return true;
+            }
+            dup.insert(n);
+        }
+        return false;
+    }
+};
