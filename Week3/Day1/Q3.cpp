ed60caf596e995b6ff4e014a7426d08923c6c3b7
rishabh248-bit <rbks0111@gmail.com>
2026-06-15 16:15:05 +0530

    Create Q3.cpp

---
+++ Week3/Day1/Q3.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(head==NULL || head->next==NULL) return head;
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast!=NULL && fast->next!=NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+};
