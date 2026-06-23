26ac0aa98ef9bbf6cb6d426b7c64f4de1dc027ce
rishabh248-bit <rbks0111@gmail.com>
2026-06-24 01:24:30 +0530

    Create Q2.cpp

---
+++ Week4/Day2/Q2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root==NULL) return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            int lvlSize = q.size();
+            vector<int> lvl;
+
+            for(int i=0; i<lvlSize; i++){
+                TreeNode* node = q.front();
+                q.pop();
+
+                lvl.push_back(node->val);
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);                   
+            }
+            ans.push_back(lvl);
+        }
+        return ans;
+    }
+};
