7569cec5127f7944a93fae83d370b9be63c58c11
rishabh248-bit <rbks0111@gmail.com>
2026-06-25 01:27:35 +0530

    Create Q2.cpp

---
+++ Week4/Day3/Q2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool validate(TreeNode* node, long long mini, long long maxi) {
+        if (node == NULL) return true;
+        if (node->val <= mini || node->val >= maxi) return false;
+
+        return validate(node->left, mini, node->val) && 
+               validate(node->right, node->val, maxi);
+    }
+    bool isValidBST(TreeNode* root) {
+        return validate(root, LLONG_MIN, LLONG_MAX);
+    }
+};
